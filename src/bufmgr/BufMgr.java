154c8b1371104388c37cec5500f8a3a1c825910a
omarelfarouk0 <omarelfarouk0@gmail.com>
2014-03-14 07:21:23 +0200

    jjj
    uytf

---
+++ src/bufmgr/BufMgr.java
@@ -0,0 +1,16 @@
+package bufmgr;
+
+import diskmgr.Page;
+import global.PageId;
+
+public class BufMgr {
+
+	public void unpinPage(PageId d, boolean f) {
+
+	}
+
+	public void pinPage(PageId d, Page g, boolean f) {
+
+	}
+
+}
